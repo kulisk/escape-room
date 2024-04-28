fc316c6e9fcc8de6561826c064bed8f6b82fe55d
kulisk <int02719@uoi.gr>
2024-04-28 19:46:07 +0300

    FinalDoor update

--- Assets/Scripts/FinalDoor.cs
+++ Assets/Scripts/FinalDoor.cs
@@ -9,8 +9,10 @@ public class FinalDoor : MonoBehaviour
     [SerializeField] private float openingSpeed = 1f; // Speed of door opening in x-axis
     private float closingStartTime; // Time when the door started closing
     private float openingStartTime; // Time when the door started opening
-    private bool isClosing = false; // Flag to indicate if the door is closing
-    private bool isOpening = false; // Flag to indicate if the door is opening
+    private bool isClosing = true; // Flag to indicate if the door is closing
+
+    private bool isMoving = false;// Αν το αντικείμενο μετακινείται ή όχι
+
 
     private void Start()
     {
@@ -20,7 +22,7 @@ public class FinalDoor : MonoBehaviour
 
     private void Update()
     {
-        if (isClosing)
+        if (isClosing==true && isMoving==true)
         {
             float elapsedTime = Time.time - closingStartTime;
             float newXPosition = doorTransform.position.x - (closingSpeed * Time.deltaTime);
@@ -29,10 +31,10 @@ public class FinalDoor : MonoBehaviour
 
             if (elapsedTime >= closingDuration)
             {
-                isClosing = false;
+                isMoving = false;
             }
         }
-        else if (isOpening)
+        else if (isClosing==false && isMoving==true)
         {
             float elapsedTime = Time.time - openingStartTime;
             float newXPosition = doorTransform.position.x + (openingSpeed * Time.deltaTime);
@@ -41,7 +43,7 @@ public class FinalDoor : MonoBehaviour
 
             if (elapsedTime >= openingDuration)
             {
-                isOpening = false;
+                isMoving = false;
             }
         }
     }
@@ -49,14 +51,22 @@ public class FinalDoor : MonoBehaviour
     // Method to start closing the door
     public void StartClosing()
     {
-        isClosing = true;
-        closingStartTime = Time.time; // Reset closing start time
+        if(isMoving==false && isClosing==false)//Έτσι ώστε να μην το ξανά καλέσει όταν ήδη κλήνει
+        {
+            isClosing = true;
+            isMoving = true;
+            closingStartTime = Time.time; // Reset closing start time
+        }
     }
 
     // Method to start opening the door
     public void StartOpening()
     {
-        isOpening = true;
-        openingStartTime = Time.time; // Reset opening start time
+        if(isMoving==true && isClosing==true)//για να ξεκινήσει να ανοίγει μόνο όταν θα είναι στην διάρκεια που κλήνει
+        {
+            isClosing = false;
+            isMoving= true;
+            openingStartTime = Time.time; // Reset opening start time
+        }
     }
 }
